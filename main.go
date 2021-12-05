17c843c2aaa3fde0239a5361a5d958bb38ebe1c8
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2021-12-05 15:23:16 +0330

    feat: glue everything together

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mehditeymorian/gofiber-sample/internal/cache"
+	"github.com/mehditeymorian/gofiber-sample/internal/config"
+	"github.com/mehditeymorian/gofiber-sample/internal/http/handler"
+)
+
+func main() {
+	cfg := config.Load()
+
+	cache := cache.New()
+
+	app := fiber.New()
+
+	v1 := app.Group("/v1")
+	handler.People{Cache: cache}.Register(v1)
+
+	if err := app.Listen(":" + cfg.Port); err != nil {
+		log.Fatalf("error starting HTTP server: %v", err)
+	}
+}
