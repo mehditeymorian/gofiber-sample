cfb21b810d9655abf9c633955a77b5822ac9f5cc
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2021-12-05 14:54:23 +0330

    refactor: remove updateAt field

--- internal/model/person.go
+++ internal/model/person.go
@@ -10,9 +10,20 @@ type Person struct {
 	PhoneNumber string
 	Age         int
 	CreatedAt   time.Time
-	UpdatedAt   time.Time
 }
 
-func NewPerson(name string, email string, phoneNumber string, age int, createdAt time.Time, updatedAt time.Time) *Person {
-	return &Person{Name: name, Email: email, PhoneNumber: phoneNumber, Age: age, CreatedAt: createdAt, UpdatedAt: updatedAt}
+func NewPerson(
+	name string,
+	email string,
+	phoneNumber string,
+	age int,
+	createdAt time.Time,
+) *Person {
+	return &Person{
+		Name:        name,
+		Email:       email,
+		PhoneNumber: phoneNumber,
+		Age:         age,
+		CreatedAt:   createdAt,
+	}
 }
